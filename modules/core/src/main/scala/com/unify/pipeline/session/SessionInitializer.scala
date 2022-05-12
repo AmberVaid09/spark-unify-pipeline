908fb17bd238fd609f71df5132a57a44c7a855d9
Amber Vaid <ambervaid09@gmail.com>
2022-05-12 21:09:05 +0530

    Added SparkSession

--- modules/core/src/main/scala/com/unify/pipeline/session/SessionInitializer.scala
+++ modules/core/src/main/scala/com/unify/pipeline/session/SessionInitializer.scala
@@ -12,14 +12,28 @@ object SessionInitializer {
    * Core responsibility
    * -> Create spark session
    * -> Read config spark_options and create spark conf
+   * -> Updates Hadoop config to support read/write to multiple cloud system.
    *
    * @param config Config
    * @return SparkSession
    */
   def create(config: Config): SparkSession = {
-    val sparkConf = new SparkConf().setAll(getSparkOptions(config))
+    val sparkConf = new SparkConf().setAll(getSparkOptions(config, Constants.SPARK_OPTIONS))
     SparkSession.builder().config(sparkConf).getOrCreate()
   }
+
+  /**
+   * Used to update Spark hadoop option
+   * -> Can be used to read data from one cloud source while writing data to different cloud source
+   *
+   * @param spark     Spark Session
+   * @param config    Config
+   * @param aliasPath Alias path found in active system for source and target config
+   */
+  def updateSparkHadoopOptions(spark: SparkSession, config: Config, aliasPath: String): Unit =
+    getSparkOptions(config, aliasPath).foreach { self =>
+      spark.sparkContext.hadoopConfiguration.set(self._1, self._2)
+    }
 
   /**
    * Returns Spark options
@@ -27,9 +41,9 @@ object SessionInitializer {
    * @param config Config
    * @return
    */
-  def getSparkOptions(config: Config): Map[String, String] = {
-    val cleanText = (value : String) => value.replaceAll("\"","")
-    config.getConfig(Constants.SPARK_OPTIONS).entrySet().asScala.flatMap { option =>
+  def getSparkOptions(config: Config, path: String): Map[String, String] = {
+    val cleanText = (value: String) => value.replaceAll("\"", "")
+    config.getConfig(path).entrySet().asScala.flatMap { option =>
       Map(cleanText(option.getKey) -> cleanText(option.getValue.render()))
     }.toMap
   }
--- modules/core/src/test/scala/com/unify/pipeline/SessionInitializerSpec.scala
+++ modules/core/src/test/scala/com/unify/pipeline/SessionInitializerSpec.scala
@@ -11,4 +11,16 @@ class SessionInitializerSpec extends BaseSpec {
     assert(spark.sql("select 1 as id").collect().map(_.getInt(0)).head == 1)
   }
 
+  "SessionInitializer" should "create and update updateSparkHadoopOptions" in {
+    System.setProperty("hadoop.home.dir", "C:\\Users\\amber\\Downloads\\hadooputils\\hadoop")
+    val spark = SessionInitializer.create(config)
+    // Updating hadoop config to gcp path
+    SessionInitializer.updateSparkHadoopOptions(spark,config,"gcp")
+    assert(spark.sparkContext.hadoopConfiguration.get("key") == "gcp_pass")
+
+    // Updating hadoop config to aws path
+    SessionInitializer.updateSparkHadoopOptions(spark,config,"aws")
+    assert(spark.sparkContext.hadoopConfiguration.get("key") == "aws_pass")
+  }
+
 }
